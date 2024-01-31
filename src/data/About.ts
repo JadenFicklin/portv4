4c5f55ab673b8fac0871034af170dc102448a42e
jaden ficklin <jadendficklin@gmail.com>
2024-01-30 19:28:24 -0700

    some file structure for project and data elements including about, experience, project archive and work

---
+++ src/data/About.ts
@@ -0,0 +1,10 @@
+const skills = { text: 'skills', link: '...' }
+const devmountain = { text: 'Devmountain', link: '...' }
+const graduate = { text: 'graduate', link: '...' }
+const Awwwards = { text: 'Awwwards.com', link: '...' }
+
+export const AboutText = [
+  `I'm a American Website Developer with ${skills} ranging from conceptual thinking to designing and developing high quality UI with structured backends`,
+  `As a dedicated ${devmountain} ${graduate} with a deep-seated passion for the front-end I thrive in supporting my team members in achieving our shared professional goals.`,
+  `When I'm not at my computer drawing rectangles through Figma or admiring the work of others at ${Awwwards}, I'm interested in new technologies and have a strong passion for drawing, watching new shows and spending time with my family.`,
+]
---
+++ src/data/Work.ts
@@ -0,0 +1,41 @@
+type Project = {
+  name: string
+  description: string
+  technologies: string[]
+  image: string
+  link: string
+}
+
+export const WorkArray: Project[] = [
+  {
+    name: 'PinaSoulmate',
+    description:
+      'A dating website built to connect foreigners and people in the Philippines. Users can create accounts, filter, like, favorite, message other users, and purchase monthly memberships.',
+    technologies: [
+      'React',
+      'Javascript',
+      'Tailwind',
+      'Stripe',
+      'Firebase',
+      'Mailgun',
+    ],
+    image: 'PinaSoulmate',
+    link: 'https://pinasoulmate.com/',
+  },
+  {
+    name: 'SVG Library',
+    description:
+      'Users can customize SVG’s using range sliders to adjust size, stroke and color of SVG’s which they can then copy the code for and use in their projects.',
+    technologies: ['React', 'Javascript', 'Tailwind', 'Zustand', 'Firebase'],
+    image: 'SVGLibrary',
+    link: 'https://svg-library.web.app/',
+  },
+  {
+    name: 'Oak and Stone',
+    description:
+      'A brochure website with emphasis on interactive UI to bring an enjoyable viewing experience for the user.',
+    technologies: ['React', 'Javascript', 'EmailJs', 'Maps API', 'Netlify'],
+    image: 'OakandStone',
+    link: 'https://oakandstone.netlify.app/',
+  },
+]
