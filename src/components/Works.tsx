a08431ea37f96eb8b5c37ac77d3a43b5eaba0d0e
JadenFicklin <jadendficklin@gmail.com>
2024-02-13 12:01:30 -0700

    works header and updates to projects styling

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,13 +1,47 @@
+import { useEffect, useRef } from 'react'
 import { WorkArray } from '~/data/work'
 import { Project } from '~/utils/Project'
 
 export const Works = () => {
+  const scrollTextRef = useRef<HTMLSpanElement>(null)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollTextRef.current) {
+        const { scrollTop } = document.documentElement
+        const scrollLeft = -scrollTop * 0.8
+        scrollTextRef.current.style.transform = `translateX(${scrollLeft}px)`
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <>
-      <div className="flex flex-wrap w-9/12 gap-32 mx-auto h-max">
-        <Project data={WorkArray[0]} orientation="left" className="mt-32" />
-        <Project data={WorkArray[1]} orientation="right" />
-        <Project data={WorkArray[2]} orientation="left" />
+      <h2 className="relative w-full mb-32 overflow-hidden text-3xl h-min mt-52 xs:text-4xl sm:text-6xl md:text-7xl lg:text-9xl">
+        <span className="opacity-0">a</span>
+        <span
+          ref={scrollTextRef}
+          className="absolute text-max whitespace-nowrap"
+          style={{ transform: 'translateX(0px)' }}
+        >
+          SELECTED PROJECTS - SELECTED PROJECTS - SELECTED PROJECTS - SELECTED
+          PROJECTS - SELECTED PROJECTS -
+        </span>
+      </h2>
+      <div className="flex flex-wrap w-9/12 gap-10 mx-auto h-max">
+        {WorkArray.map((work, index) => (
+          <Project
+            key={index}
+            data={work}
+            orientation={index % 2 === 0 ? 'left' : 'right'}
+          />
+        ))}
       </div>
     </>
   )
--- src/utils/Project.tsx
+++ src/utils/Project.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { cn } from '~/utils/cn'
 
 type ProjectType = {
@@ -27,8 +28,8 @@ export const Project: React.FC<ProjectProps> = ({
   orientation,
   className,
 }) => {
-  console.log(orientation)
   const { name, description, technologies, image, link } = data
+  const [flagAnimationState, setFlagAnimationState] = useState('')
 
   const divStyle = {
     backgroundImage: `url(${image})`,
@@ -36,12 +37,36 @@ export const Project: React.FC<ProjectProps> = ({
     backgroundSize: 'cover',
   }
 
+  const handleAnimateFlag = () => {
+    setFlagAnimationState('')
+    setTimeout(() => {
+      setFlagAnimationState('dot')
+    }, 1000)
+    setTimeout(() => {
+      setFlagAnimationState('flag')
+    }, 2000)
+  }
+
+  useEffect(() => {
+    handleAnimateFlag()
+  }, [])
+
   return (
     <>
-      <div className={cn('w-full py-10 h-[65vh] flex flex-wrap', className)}>
-        <div className="w-1/2">
+      <div
+        className={cn(
+          'w-full py-10 h-[65vh] lg:h-[85vh] grid grid-cols-2',
+          className,
+        )}
+      >
+        {/* image section */}
+        <div
+          className={cn(
+            orientation === 'right' && 'order-2 flex justify-end scale-90',
+          )}
+        >
           <a
-            className="relative flex items-center w-10/12 h-full"
+            className="relative flex items-center w-10/12 h-full "
             href={link}
             target="_blank"
             rel="noreferrer"
@@ -55,12 +80,59 @@ export const Project: React.FC<ProjectProps> = ({
             />
           </a>
         </div>
-        <div className="w-1/2">
-          <div>{name}</div>
-          <div>{description}</div>
-          {technologies.map((tech, index) => (
-            <div key={index}>{tech}</div>
-          ))}
+        {/* text and flag section */}
+        <div className="flex flex-wrap">
+          <div
+            className={cn('w-3/12', orientation.includes('right') && 'order-2')}
+          ></div>
+          <div className="flex flex-wrap content-center w-9/12">
+            <div className="relative w-full h-max">
+              {/* animated flag */}
+              <div
+                className={cn(
+                  'duration-1000  ease-in-out absolute bottom-0 w-1 h-full bg-max',
+                  !flagAnimationState && 'h-0',
+                  orientation.includes('left') && '-left-20',
+                  orientation.includes('right') && '-right-20',
+                )}
+              >
+                {/* dot */}
+                <div
+                  className={cn(
+                    'absolute rounded-full -left-1 -bottom-1 size-3 bg-max opacity-0 duration-100',
+                    (flagAnimationState === 'dot' ||
+                      flagAnimationState === 'flag') &&
+                      'opacity-100',
+                  )}
+                ></div>
+
+                <div
+                  className={cn(
+                    'duration-300 ease-in-out absolute top-0 h-4 w-0 bg-max',
+                    flagAnimationState === 'flag' && 'w-7',
+                    orientation.includes('left') && 'left-0',
+                    orientation.includes('right') && 'right-0',
+                  )}
+                ></div>
+              </div>
+              {/* text */}
+              <div className="text-4xl">{name}</div>
+              <div className="py-8">
+                {description}
+                {orientation}
+              </div>
+              <div className="flex flex-wrap gap-2">
+                {technologies.map((tech, index) => (
+                  <div
+                    key={index}
+                    className="px-3 py-1 text-sm rounded-full text-min bg-max"
+                  >
+                    {tech}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
